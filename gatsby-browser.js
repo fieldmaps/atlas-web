ebc46beb9864a9fa12c4ac823135fc0b36b94ec3
Max Malynowsky <maxmalynowsky@gmail.com>
2020-01-26 23:06:13 +0100

    redirect when service worker removes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,30 +1,18 @@
-import React from 'react';
-import { navigate } from 'gatsby';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import WrapRootElement from './src/utils/wrap-root-element';
 import './src/styles/styles.sass';
 
-const ELEMENT_ID = 'gatsby-browser-service-worker-notification';
-
-const addProgressBar = () => {
-  if (!document.getElementById(ELEMENT_ID)) {
-    const elem = document.createElement('progress');
-    elem.id = ELEMENT_ID;
-    elem.className = 'progress is-small is-primary is-fixed-top';
-    document.body.prepend(elem);
-  }
+export const onServiceWorkerActive = () => {
+  const slug = window.location.hostname.split('-')[0];
+  window.location.replace(`https://atlas.fieldmaps.io/maps/${slug}/`);
 };
-
-export const onServiceWorkerActive = () => window.location.reload();
 
 export const onServiceWorkerUpdateFound = () => {
-  if (navigator.serviceWorker.controller) addProgressBar();
+  const slug = window.location.hostname.split('-')[0];
+  window.location.replace(`https://atlas.fieldmaps.io/maps/${slug}/`);
 };
 
-export const onServiceWorkerUpdateReady = () =>
-  navigate(window.location.pathname);
-
-export const wrapRootElement = ({ element }) => (
-  <WrapRootElement element={element} />
-);
+export const onServiceWorkerUpdateReady = () => {
+  const slug = window.location.hostname.split('-')[0];
+  window.location.replace(`https://atlas.fieldmaps.io/maps/${slug}/`);
+};
